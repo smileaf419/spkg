baed8d58d4dd9de240a914f6c7e8d285a5e01d9f
smileaf419 <smileaf@me.com>
2023-08-28 14:16:36 -0500

    Enable an empty world build for temptools

--- spkg-tools/chroot-finalize.sh
+++ spkg-tools/chroot-finalize.sh
@@ -4,7 +4,7 @@ BOOTSTRAP_STAGE=$1; shift
 if [[ $BOOTSTRAP_STAGE == *3* ]]; then
 #	$PKG_DB_DIR/spkg-tools/locales
 	echo " * Installing Temporary tools"
-	spkg $@ @temptools
+	spkg $@ -e @temptools
 fi
 if [[ $BOOTSTRAP_STAGE == *4* ]]; then
 	echo " * Installing the basic system"
