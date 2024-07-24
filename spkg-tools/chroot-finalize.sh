d4b703f9458d81a6c3daf2f95a3453b9ab8a7beb
smileaf419 <smileaf@me.com>
2024-07-24 10:58:25 -0500

    Change variable name from BOOTSTRAP_STAGE to BOOTSTRAP_STAGES and set a BOOTSTRAP_STAGE to the current stage create a lock file that we can check for the existance of to test the success of our bootstrap

--- spkg-tools/chroot-finalize.sh
+++ spkg-tools/chroot-finalize.sh
@@ -1,12 +1,15 @@
 #!/bin/bash -e
 PKG_DB_DIR=$1; shift
-BOOTSTRAP_STAGE=$1; shift
-if [[ $BOOTSTRAP_STAGE == *3* ]]; then
+BOOTSTRAP_STAGES=$1; shift
+if [[ $BOOTSTRAP_STAGES == *3* ]]; then
+	BOOTSTRAP_STAGE=3
 #	$PKG_DB_DIR/spkg-tools/locales
 	echo " * Installing Temporary tools"
 	spkg $@ -e @temptools || exit 1
 fi
-if [[ $BOOTSTRAP_STAGE == *4* ]]; then
+if [[ $BOOTSTRAP_STAGES == *4* ]]; then
+	BOOTSTRAP_STAGE=4
 	echo " * Installing the basic system"
 	spkg $@ -e @basicsystem || exit 1
 fi
+rm /tmp/.spkg-bootstrap.lock
