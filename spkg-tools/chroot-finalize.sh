969f29ce8f2025cedd415572df009323472a3b18
smileaf419 <smileaf@me.com>
2024-08-06 10:01:47 -0500

    Don't install locales yet, we'll do that after we've entered the chroot environment in stage 3

--- spkg-tools/chroot-finalize.sh
+++ spkg-tools/chroot-finalize.sh
@@ -3,7 +3,7 @@ PKG_DB_DIR=$1; shift
 BOOTSTRAP_STAGES=$1; shift
 if [[ $BOOTSTRAP_STAGES == *3* ]]; then
 	BOOTSTRAP_STAGE=3
-#	$PKG_DB_DIR/spkg-tools/locales
+	$PKG_DB_DIR/spkg-tools/locales
 	echo " * Installing Temporary tools"
 	spkg $@ -e @temptools || exit 1
 fi
