d0fc0a4b4cdb63805768139add524cf3f22e3fce
smileaf <smileaf@me.com>
2024-09-23 13:28:50 +0000

    Fix path to the locales script

--- chroot-finalize.sh
+++ chroot-finalize.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -e
+source /etc/spkg.conf
+
 PKG_DB_DIR=$1; shift
 BOOTSTRAP_STAGES=$1; shift
 if [[ $BOOTSTRAP_STAGES == *3* ]]; then
 	BOOTSTRAP_STAGE=3
-	$PKG_DB_DIR/spkg-tools/locales
+	$INSTALL_PATH/locales
 	echo " * Installing Temporary tools"
 	spkg $@ -e @temptools || exit 1
 fi
